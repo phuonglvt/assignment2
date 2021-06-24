e742f0eb54e6ebe0f74c8071797c5738879a137d
Phuong <48084107+phuonglvt@users.noreply.github.com>
2021-06-24 14:34:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,49 @@
 <?php
-	$login_user = $_POST['username'];
-	$login_pdw = $POST['password'];
-	
-	$pg_heroku = pg_connect($conn_string);
-	$sql = "SELECT * FROM user_table WHERE username = '$login_user' and password = '$login_pdw'";
-	$result = pg_query($pg_heroku,$sql);
-	
-	$rum_rows = pg_num_rows($result);
-	
-	if($num_rows == 1)
+	include("local_config.php");
+	#ob_start();
+	session_start();
+	$error = "";
+	if($_SERVER["REQUEST_METHOD"] == "POST") 
 	{
-		$row = pg_fetch_array($result, 0);
-	}
-?>	
+		// username and password sent from form 
+		$login_user = $_POST['username'];
+		$login_pwd = $_POST['password']; 
+		# Create connection to Heroku Postgres
+		$pg_heroku = pg_connect($conn_string);
+		# Get data by query
+		$sql = "SELECT * FROM user_table WHERE username = '$login_user' and password = '$login_pwd'";
+		$result = pg_query($pg_heroku,$sql);
+		
+		$num_rows = pg_num_rows($result);
+
+		// If result matched $login_user and $login_pwd, table row must be 1 row
+		if($num_rows == 1) 
+		{
+			$row = pg_fetch_array($result, 0);
+			$_SESSION["name"] = $row['username'];
+			$_SESSION["shop"] = $row['shop_name'];
+			$shop = $row['shop_name'];
+			if ($shop == "DIRECTOR")
+			{
+				header("location: director.php");
+			}
+			else
+			{
+				$host  = $_SERVER['HTTP_HOST'];
+				$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+				$extra = 'crud.php';
+				#header("Location: http://$host$uri/$extra?shop=$shop");
+				header("location: crud.php/");
+			}
+		}
+		else 
+		{
+			$error = "Your Login Name or Password is invalid";
+		}
+		pg_close();
+    }	
+?>
+
 <html> 
 
    <head>
@@ -57,14 +88,9 @@
                   <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
                   <input type = "submit" value = "Submit"/><br />
 				</form>
-                <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
-               
-					
-            </div>
-				
-         </div>
-			
+                <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>             					
+            </div>				
+         </div>			
       </div>
-
    </body>
 </html>
