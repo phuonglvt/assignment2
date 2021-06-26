2390e28aa930895055897b818252032bd5da234d
Phuong <48084107+phuonglvt@users.noreply.github.com>
2021-06-26 17:40:06 +0700

    Add files via upload

---
+++ main/danhmuc.php
@@ -0,0 +1,32 @@
+<h3>Danh mục sản phẩm</h3>
+					<ul class="product_list">
+						<li>
+						<a href="">
+							<img src="doll.jpg">
+							<p class="title_product">Tên sản phẩm: doll</P>
+							<p class="price_product">Giá: 200000 vnd</p>
+						</a>
+						</li>
+					</ul>
+					<ul class="product_list">
+						<li>
+							<img src="doll.jpg">
+							<p class="title_product">Tên sản phẩm: doll</P>
+							<p class="price_product">Giá: 200000 vnd</p>
+						</li>
+						<li>
+							<img src="doll.jpg">
+							<p class="title_product">Tên sản phẩm: doll</P>
+							<p class="price_product">Giá: 200000 vnd</p>
+						</li>
+						<li>
+							<img src="doll.jpg">
+							<p class="title_product">Tên sản phẩm: doll</P>
+							<p class="price_product">Giá: 200000 vnd</p>
+						</li>
+						<li>
+							<img src="doll.jpg">
+							<p class="title_product">Tên sản phẩm: doll</P>
+							<p class="price_product">Giá: 200000 vnd</p>
+						</li>
+					</ul>
---
+++ sidebar/sidebar.php
@@ -0,0 +1,8 @@
+<div class="sidebar">
+	<ul class="menu_doc">
+		<li><a href="index.php?quanly=danhmucsanpham&id=1">Shop</a></li>
+		<li><a href="index.php?quanly=danhmucsanpham&id=2">Babie</a></li>
+		<li><a href="index.php?quanly=danhmucsanpham&id=3">Car</a></li>
+		<li><a href="index.php?quanly=danhmucsanpham&id=4">Lego</a></li>
+	</ul>
+</div>
