30e8c3a645270eff081305dbf0c89a5e1468eba0
Phuong <48084107+phuonglvt@users.noreply.github.com>
2021-07-02 01:21:02 +0700

    Update dbconnect.php
    fix connect

--- method/dbconnect.php
+++ method/dbconnect.php
@@ -1,9 +1,10 @@
 <?php
-        $host ="ec2-54-167-152-185.compute-1.amazonaws.com";
-        $user ="kmrkvgmgcjpdfm";
-        $password ="858848643e0cd1fec1f34c4d79793fb8ff28b39c7a4d36d9dad7c25b214c6ca6";
-        $db ="dbcqmns0qplkj3";
-        $mysqli = new mysqli($host,$user ,$password,$db);
+        $mysqli = pg_connect(
+	host=ec2-54-167-152-185.compute-1.amazonaws.com
+	dbname=dbcqmns0qplkj3
+        user=kmrkvgmgcjpdfm
+        password=858848643e0cd1fec1f34c4d79793fb8ff28b39c7a4d36d9dad7c25b214c6ca6
+        );
 
 // Check connection
 if ($mysqli -> connect_errno) {
