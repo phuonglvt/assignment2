17e64888c969e0b152388c62525976d4b7597c20
Phuong <48084107+phuonglvt@users.noreply.github.com>
2021-07-02 00:23:49 +0700

    Delete local_config.php

--- local_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$host_db = "ec2-54-167-152-185.compute-1.amazonaws.com";
-	$db_name = "dbcqmns0qplkj3";
-	$db_user = "kmrkvgmgcjpdfm";
-	$db_pass = "858848643e0cd1fec1f34c4d79793fb8ff28b39c7a4d36d9dad7c25b214c6ca6";
-	
-	$conn_string = "host = $host_db port = 5432 dbname = $db_name user = $db_user password = $db_pass";
-	$pg_conn = pg_connect($conn_string);
-?>
