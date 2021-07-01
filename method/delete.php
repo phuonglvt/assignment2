10f34f129bf497c412b67b7e9ab7d3359dcbd26b
Phuong <48084107+phuonglvt@users.noreply.github.com>
2021-07-02 01:38:52 +0700

    Delete method directory

--- method/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require("../db/dbconnect.php");
-
-$query = "DELETE FROM register where id ='".$_POST['id']."' ";
-$result = mysqli_query($mysqli,$query);
-$mysqli ->close();
-header("location: ../admin/admin.php");
-?>
--- method/insert.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("../db/dbconnect.php");
-
-$query = "INSERT INTO `register` (`id`, `email`, `name`, `pwd`) VALUES (NULL, '".$_POST['email']."', '".$_POST['name']."', '".$_POST['pwd']."');";
-$result = mysqli_query($mysqli,$query);
-$mysqli ->close();
-header("location: ../admin/admin.php");
-?>
